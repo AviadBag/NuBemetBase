8a23964597d9e5dabb2bdb8d981d3317348e2f95
Aviad Bagno <aviad.bagno@gmail.com>
2021-11-20 22:18:20 +0200

    Using /* */ instead of // for comments, because it is c90 :(

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-// If TEST is defined, it means that we are compiled for unit-testing, and we already have one main() - the main
-// of the tester, and no more than one main is allowed.
+/*
+ If TEST is defined, it means that we are compiled for unit-testing, and we already have one main() - the main
+ of the tester, and no more than one main is allowed.
+ */
 #ifndef TEST
 int main()
 {
